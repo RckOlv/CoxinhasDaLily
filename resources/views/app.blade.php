5166b842987f651b82b188b05a2668a5f5396e85
rckolv <rckolv9@gmail.com>
2026-07-26 01:59:08 -0300

    Fix app name in browser tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Coxinhas da Lily') }}</title>
         <link rel="icon" type="image/png" href="{{ asset('img/logolilyconfondo.png') }}">
 
         <!-- Meta tags para compartir en redes sociales -->
