6090fb32096be0a6200d325619c06572c202a764
rckolv <rckolv9@gmail.com>
2026-07-26 02:09:06 -0300

    Fix session and proxy config for Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,8 +20,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
-
-        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
